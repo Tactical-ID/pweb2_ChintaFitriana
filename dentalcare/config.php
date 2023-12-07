ef977ffc4a8b08525fc38df213feedea0c0554d8
pemanentomat <118662112+chintafitriana@users.noreply.github.com>
2023-12-07 11:00:43 +0700

    Add files via upload

---
+++ dentalcare/config.php
@@ -0,0 +1,17 @@
+<?php
+
+class database{
+    var $host = "localhost";
+    var $username = "root";
+    var $password = "";
+    var $db = "dentalcare";
+    var $koneksi;
+
+    function getKoneksi(){
+        $this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->db);
+        if ($this->koneksi->connect_error) {
+            die("Koneksi database gagal: " . $this->koneksi->connect_error);
+        }
+        return $this->koneksi;
+    }
+}
